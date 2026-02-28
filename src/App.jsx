05ce9174a6fb5fb52ba9e0d8fd39f6981a01e2c6
RuslanChekhuta <ruslan.gpt2025@gmail.com>
2026-02-28 23:09:45 +0100

    Добавить компонент счетчика с хуком useCounter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import useCounter from "./useCounter";
+
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [count, increment, decrement, reset] = useCounter(10, 5);
+
+  return (
+    <>
+      <h1>Счетчик: {count}</h1>
+      <button onClick={increment}>Прибавить</button>
+      <button onClick={decrement}>Убавить</button>
+      <button onClick={reset}>Сбросить</button>
+    </>
+  );
 }
 
 export default App;
