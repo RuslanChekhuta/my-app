ef0d5e8905692d6b95a35442c41328a6bcdf509a
RuslanChekhuta <ruslan.gpt2025@gmail.com>
2026-03-03 19:35:40 +0100

    реализовать ленивую загрузку компонентов с использованием React.lazy и Suspense

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,41 @@
-import "./App.css";
+import { lazy, Suspense, useState } from "react";
+
+const TodoComponent = lazy(() => import("../TodoComponent.jsx"));
+const TextComponent = lazy(() => import("../TextComponent.jsx"));
 
 function App() {
-  return <></>;
+  const [showText, setShowText] = useState(false);
+  const [showTodo, setShowTodo] = useState(false);
+
+  return (
+    <div>
+      <div>
+        <h2>1. Загрузка компонента</h2>
+        <button
+          onClick={() => {
+            setShowText(!showText);
+          }}
+        >
+          {showText ? "Скрыть" : "Показать"} компонент
+        </button>
+        <Suspense fallback={<div>Компонент загружается</div>}>
+          {showText && <TextComponent />}
+        </Suspense>
+
+        <h2>2. Загрузка компонента списка задач</h2>
+        <button
+          onClick={() => {
+            setShowTodo(!showTodo);
+          }}
+        >
+          {showTodo ? "Скрыть" : "Показать"} список задач
+        </button>
+        <Suspense fallback={<div>Компонент загружается</div>}>
+          {showTodo && <TodoComponent />}
+        </Suspense>
+      </div>
+    </div>
+  );
 }
 
 export default App;
